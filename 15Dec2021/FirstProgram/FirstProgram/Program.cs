e2d5568429b0f86165e6d80259ac798bef5d73ef
Sai Allani <allanisaikumar.dotnet@outlook.com>
2021-12-17 07:32:26 +0530

    Day 1

---
+++ 15Dec2021/FirstProgram/FirstProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿// namespaced declarations
+using System;
+
+
+// Start of the namespace
+namespace FirstProgram
+{
+    // Class
+    class Program
+    {
+        // Main method - entry point of the application.
+        public static void Main(string[] args)
+        {
+            // Write to Console
+            Console.WriteLine("Welcome to C# Sessions");
+            //Console.
+            // Writting with help of namespace
+            Console.WriteLine("Welcome to C# Sessions");
+
+            // This does not need name space inclusion
+            Console.WriteLine("Write using the Fully qualified name");
+
+            Console.ReadKey();
+        }
+    }
+}
